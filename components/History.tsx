f41316ce6c7a2a465adcdb0efc4de77d94e1d06e
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2023-09-25 10:09:20 +0100

    first commit

---
+++ components/History.tsx
@@ -0,0 +1,13 @@
+
+
+
+const History = () => {
+    return (
+        <div className="w-1/5 h-screen overflow-y-auto bg-black/50">
+            History
+        </div>
+    )
+}
+
+
+export default History
---
+++ components/Input.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+
+const Input = () => {
+  return (
+    <div className="w-auto h-screen">
+        <div className="">
+
+        </div>
+        <div className="">
+           <input type="text" placeholder="Send a message" className="" ></input> 
+           
+        </div>
+    </div>
+  )
+}
+
+export default Input
